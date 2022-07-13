05a13a84fccf88bc19373046a9285b9e9dde049e
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-07-13 22:04:00 +0200

    Added tokens and trivias

--- src/parser/trivias.rs
+++ src/parser/trivias.rs
@@ -1,4 +1,7 @@
 
 pub enum Trivia {
-    Whitespace(u32, u32)
+    Whitespace(u32, u32, char),
+    Newline(u32, u32, char, char),
+    LineContinuation(u32, u32),
+    Comment(u32, u32, Box<String>),
 }
