23625979e13d267da238667c08a7d03b889b0894
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-07-19 23:02:42 +0200

    Template for all blocks rules

--- src/parser/blocks.rs
+++ src/parser/blocks.rs
@@ -11,6 +11,16 @@ pub trait Blocks {
     fn parse_blocks_eval_input(&self) -> Box<ASTNode>;
     fn parse_blocks_file_input(&self) -> Box<ASTNode>;
     fn parse_blocks_single_input(&self) -> Box<ASTNode>;
+    fn parse_blocks_decorator(&self) -> Box<ASTNode>;
+    fn parse_blocks_decorators(&self) -> Box<ASTNode>;
+    fn parse_blocks_decorated(&self) -> Box<ASTNode>;
+    fn parse_blocks_async_func_def(&self) -> Box<ASTNode>;
+    fn parse_blocks_func_def(&self) -> Box<ASTNode>;
+    fn parse_blocks_parameters(&self) -> Box<ASTNode>;
+    fn parse_blocks_typed_args_list(&self) -> Box<ASTNode>;
+    fn parse_blocks_tfp_def_assign(&self) -> Box<ASTNode>;
+    fn parse_blocks_tfp_def(&self) -> Box<ASTNode>;
+    fn parse_blocks_class_def(&self) -> Box<ASTNode>;
 }
 
 
@@ -24,6 +34,46 @@ impl Blocks for PythonCoreParser {
     }
 
     fn parse_blocks_single_input(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_decorator(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_decorators(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_decorated(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_async_func_def(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_func_def(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_parameters(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_typed_args_list(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_tfp_def_assign(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_tfp_def(&self) -> Box<ASTNode> {
+        Box::new( ASTNode::Empty )
+    }
+
+    fn parse_blocks_class_def(&self) -> Box<ASTNode> {
         Box::new( ASTNode::Empty )
     }
 }
