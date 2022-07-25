2c95a5675e75ada1923121cf357d16047d87a0a8
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-07-26 00:50:42 +0200

    Completed first version of reserved keyword or atomname analyzer.

--- src/parser/tokenizer.rs
+++ src/parser/tokenizer.rs
@@ -30,7 +30,7 @@ impl PythonCoreTokenizer {
         0
     }
 
-    /// This method checks for reserved keywords or atom name literal and provides token with position and trivias collected in fron ot token
+    /// This method checks for reserved keywords or atom name literal and provides token with position and trivia collected in fron ot token
     fn is_reserved_keyword(start_pos: &u32, end_pos: &u32, trivia: Option<Box<Vec<Box<Trivia>>>>, buffer: &str) -> Token {
         match &*buffer {
             "False" => Token::PyFalse(*start_pos, *end_pos, trivia),
@@ -38,7 +38,37 @@ impl PythonCoreTokenizer {
             "True" => Token::PyTrue(*start_pos, *end_pos, trivia),
             "and" => Token::PyAnd(*start_pos, *end_pos, trivia),
             "as" => Token::PyAs(*start_pos, *end_pos, trivia),
-            _ => Token::Empty
+            "assert" => Token::PyAssert(*start_pos, *end_pos, trivia),
+            "async" => Token::PyAsync(*start_pos, *end_pos, trivia),
+            "await" => Token::PyAwait(*start_pos, *end_pos, trivia),
+            "break" => Token::PyBreak(*start_pos, *end_pos, trivia),
+            "class" => Token::PyClass(*start_pos, *end_pos, trivia),
+            "continue" => Token::PyContinue(*start_pos, *end_pos, trivia),
+            "def" => Token::PyDef(*start_pos, *end_pos, trivia),
+            "del" => Token::PyDel(*start_pos, *end_pos, trivia),
+            "elif" => Token::PyElif(*start_pos, *end_pos, trivia),
+            "else" => Token::PyElse(*start_pos, *end_pos, trivia),
+            "except" => Token::PyExcept(*start_pos, *end_pos, trivia),
+            "finally" => Token::PyFinally(*start_pos, *end_pos, trivia),
+            "for" => Token::PyFor(*start_pos, *end_pos, trivia),
+            "from" => Token::PyFrom(*start_pos, *end_pos, trivia),
+            "global" => Token::PyGlobal(*start_pos, *end_pos, trivia),
+            "if" => Token::PyIf(*start_pos, *end_pos, trivia),
+            "import" => Token::PyImport(*start_pos, *end_pos, trivia),
+            "in" => Token::PyIn(*start_pos, *end_pos, trivia),
+            "is" => Token::PyIs(*start_pos, *end_pos, trivia),
+            "lambda" => Token::PyLambda(*start_pos, *end_pos, trivia),
+            "nonlocal" => Token::PyNonLocal(*start_pos, *end_pos, trivia),
+            "not" => Token::PyNot(*start_pos, *end_pos, trivia),
+            "or" => Token::PyOr(*start_pos, *end_pos, trivia),
+            "pass" => Token::PyPass(*start_pos, *end_pos, trivia),
+            "raise" => Token::PyRaise(*start_pos, *end_pos, trivia),
+            "return" => Token::PyReturn(*start_pos, *end_pos, trivia),
+            "try" => Token::PyTry(*start_pos, *end_pos, trivia),
+            "while" => Token::PyWhile(*start_pos, *end_pos, trivia),
+            "with" => Token::PyWith(*start_pos, *end_pos, trivia),
+            "yield" => Token::PyYield(*start_pos, *end_pos, trivia),
+            _ => Token::AtomName(*start_pos, *end_pos, trivia, Box::new((*buffer.to_string()).parse().unwrap()))
         }
     }
 }
