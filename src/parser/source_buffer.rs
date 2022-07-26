7962983b405d0accabe570374b2d528bcc5b0740
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-07-26 22:14:38 +0200

    First three unittests for SourceBuffer implemented and passed.

--- src/parser/source_buffer.rs
+++ src/parser/source_buffer.rs
@@ -18,16 +18,16 @@ impl SourceBuffer {
     pub fn get_char(&self) -> &char {
         let index_local = *&self.index as usize;
         let max = *&self.source_buffer.len() as usize;
-        if (index_local < max) { &*&(self.source_buffer[index_local]) } else { &*&(self.source_buffer[max - 2]) }
+        if (index_local < max) { &*&(self.source_buffer[index_local]) } else { &'\0' }
     }
 
     /// Returns a peek into current position and next two position in source buffer, without any advance. At end it will just return eof as character.
     pub fn peek_three_chars(&self) -> ( &char, &char, &char ) {
         let index_local = *&self.index as usize;
         let max = *&self.source_buffer.len() as usize;
-        let a = if (index_local < max) { &*&(self.source_buffer[index_local]) } else { &*&(self.source_buffer[max - 2]) };
-        let b = if ((index_local + 1) < max) { &*&(self.source_buffer[index_local + 1]) } else { &*&(self.source_buffer[max - 2]) };
-        let c = if ((index_local + 2) < max) { &*&(self.source_buffer[index_local + 2]) } else { &*&(self.source_buffer[max - 2]) };
+        let a = if (index_local < max) { &*&(self.source_buffer[index_local]) } else { &'\0' };
+        let b = if ((index_local + 1) < max) { &*&(self.source_buffer[index_local + 1]) } else { &'\0' };
+        let c = if ((index_local + 2) < max) { &*&(self.source_buffer[index_local + 2]) } else { &'\0' };
         ( &a, &b, &c )
     }
 
@@ -46,4 +46,58 @@ impl SourceBuffer {
         self.source_buffer.len()
     }
 
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::source_buffer::SourceBuffer;
+
+    #[test]
+    fn read_characters_from_source_buffer() {
+        let mut buffer = Box::new( SourceBuffer::new("def a(): pass".to_string()) );
+        assert_eq!(buffer.get_position(), &0u32);
+        assert_eq!(buffer.get_char(), &'d');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &'e');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &'f');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &' ');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &'a');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &'(');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &')');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &':');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &' ');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &'p');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &'a');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &'s');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &'s');
+        buffer.advance();
+        assert_eq!(buffer.get_char(), &'\0');
+    }
+
+    #[test]
+    fn peek_next_three_characters_and_check_position() {
+        let mut buffer = Box::new( SourceBuffer::new("def a(): pass".to_string()) );
+        let ( a, b, c ) = buffer.peek_three_chars();
+        assert_eq!(a, &'d');
+        assert_eq!(b, &'e');
+        assert_eq!(c, &'f');
+        assert_eq!(buffer.get_position(), &0u32);
+    }
+
+    #[test]
+    fn length_of_source_buffer_content() {
+        let mut buffer = Box::new( SourceBuffer::new("def a(): pass".to_string()) );
+        assert_eq!(buffer.count(), (13u32 as usize));
+    }
 }
