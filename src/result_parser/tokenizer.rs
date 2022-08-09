98d28459450c4b5c77858f076499be83e0433ada
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-08-09 19:14:33 +0200

    Started on new version Tokenizer with get methods.

--- src/result_parser/tokenizer.rs
+++ src/result_parser/tokenizer.rs
@@ -1,6 +1,7 @@
 
 use crate::result_parser::source_buffer::SourceBuffer;
 use crate::result_parser::source_buffer::SourceBufferFunctionality;
+use crate::Token;
 
 pub struct PythonCoreTokenizer {
     source_buffer: Box<SourceBuffer>
@@ -9,6 +10,8 @@ pub struct PythonCoreTokenizer {
 
 trait Tokenizer {
     fn new(buffer: String) -> PythonCoreTokenizer;
+    fn get_symbol() -> Result<Token, String>;
+    fn get_position() -> u32;
 }
 
 
@@ -17,5 +20,13 @@ impl Tokenizer for PythonCoreTokenizer {
         PythonCoreTokenizer {
             source_buffer: Box::new( SourceBuffer::new(buffer) )
         }
+    }
+
+    fn get_symbol() -> Result<Token, String> {
+        Ok(Token::Empty)
+    }
+
+    fn get_position() -> u32 {
+        0u32
     }
 }
