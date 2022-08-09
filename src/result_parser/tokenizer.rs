73e66c25681db1b6eb1ec72fe5dedcefb26721e9
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-08-09 20:03:36 +0200

    Started on floordiv and div operator.

--- src/result_parser/tokenizer.rs
+++ src/result_parser/tokenizer.rs
@@ -51,6 +51,27 @@ impl Tokenizer for PythonCoreTokenizer {
                 Ok(Box::new( Token::PyMul(self.token_start_position, self.source_buffer.get_position(),
                                             match trivia_collector.len() { 0 => None, _ => Some( trivia_collector ) } ) ))
             },
+            ( '/', '/', '=' ) => {
+                for i in 1 ..= 3 { let _ = self.source_buffer.advance(); }
+                Ok(Box::new( Token::PyFloorDivAssign(self.token_start_position, self.source_buffer.get_position(),
+                                                  match trivia_collector.len() { 0 => None, _ => Some( trivia_collector ) } ) ))
+            },
+            ( '/', '/', _ ) => {
+                for i in 1 ..= 2 { let _ = self.source_buffer.advance(); }
+                Ok(Box::new( Token::PyFloorDiv(self.token_start_position, self.source_buffer.get_position(),
+                                            match trivia_collector.len() { 0 => None, _ => Some( trivia_collector ) } ) ))
+            },
+            ( '/', '=', _ ) => {
+                for i in 1 ..= 2 { let _ = self.source_buffer.advance(); }
+                Ok(Box::new( Token::PyDivAssign(self.token_start_position, self.source_buffer.get_position(),
+                                                match trivia_collector.len() { 0 => None, _ => Some( trivia_collector ) } ) ))
+            },
+            ( '/', _ , _ ) => {
+                let _ = self.source_buffer.advance();
+                Ok(Box::new( Token::PyDiv(self.token_start_position, self.source_buffer.get_position(),
+                                          match trivia_collector.len() { 0 => None, _ => Some( trivia_collector ) } ) ))
+            },
+
             _ => {
                 let txt = format!( "Lexical error at ({}), found '{}'", self.source_buffer.get_position(), self.source_buffer.get_char() );
                 Err(txt)
