defcda50557fbbd8d210b4df0e45ee91c1eed6ea
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-08-12 20:52:43 +0200

    Added equal, not equal and assign UnitTests for tokenizer.

--- src/result_parser/tokenizer.rs
+++ src/result_parser/tokenizer.rs
@@ -836,4 +836,46 @@ mod tests {
             Err( e ) => assert!(false)
         }
     }
+
+    #[test]
+    fn tokenizer_operator_or_delimiter_assign() {
+        let mut tokenizer = Box::new( PythonCoreTokenizer::new( "==".to_string() ) );
+        match tokenizer.get_symbol() {
+            Ok( s ) => {
+                match *s {
+                    Token::PyEqual( 0u32, 2u32, None) => assert!(true),
+                    _ => assert!(false)
+                }
+            }
+            Err( e ) => assert!(false)
+        }
+    }
+
+    #[test]
+    fn tokenizer_operator_or_delimiter_not_equal_default() {
+        let mut tokenizer = Box::new( PythonCoreTokenizer::new( "!=".to_string() ) );
+        match tokenizer.get_symbol() {
+            Ok( s ) => {
+                match *s {
+                    Token::PyNotEqual( 0u32, 2u32, None) => assert!(true),
+                    _ => assert!(false)
+                }
+            }
+            Err( e ) => assert!(false)
+        }
+    }
+
+    #[test]
+    fn tokenizer_operator_or_delimiter_equal() {
+        let mut tokenizer = Box::new( PythonCoreTokenizer::new( "=".to_string() ) );
+        match tokenizer.get_symbol() {
+            Ok( s ) => {
+                match *s {
+                    Token::PyAssign( 0u32, 1u32, None) => assert!(true),
+                    _ => assert!(false)
+                }
+            }
+            Err( e ) => assert!(false)
+        }
+    }
 }
