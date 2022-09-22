639503ef647f0f7a9fc0794f17a956faf8f2ee8d
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-09-22 21:21:02 +0200

    Implemented rule for decorator statement.

--- src/parser/blocks.rs
+++ src/parser/blocks.rs
@@ -201,7 +201,71 @@ impl Blocks for PythonCoreParser {
     }
 
     fn parse_blocks_decorator(&mut self) -> Result<Box<ASTNode>, String> {
-        todo!()
+        let start_pos = self.lexer.get_position();
+        match self.symbol.clone() {
+            Ok(s) => {
+                match &*s {
+                    Token::PyMatrice(..) => {
+                        let symbol1 = s;
+                        let _ = self.advance();
+                        let left_node = self.parse_statements_dotted_name()?;
+                        let mut symbol2 : Option<Box<Token>> = None;
+                        let mut right_node : Option<Box<ASTNode>> = None;
+                        let mut symbol3 : Option<Box<Token>> = None;
+                        match self.symbol.clone() {
+                            Ok(s2) => {
+                                match &*s2 {
+                                    Token::PyLeftParen(..) => {
+                                        symbol2 = Some( s2 );
+                                        let _ = self.advance();
+                                        match self.symbol.clone() {
+                                            Ok(s3) => {
+                                                match &*s3 {
+                                                    Token::PyRightParen(..) => {},
+                                                    _ => {
+                                                        right_node = Some( self.parse_expressions_arglist()? );
+                                                    }
+                                                }
+                                            },
+                                            _ => return Err(format!("SyntaxError at {}: Expecting symbol in functional input!", start_pos))
+                                        }
+                                        match self.symbol.clone() {
+                                            Ok(s4) => {
+                                                match &*s4 {
+                                                    Token::PyRightParen(..) => {
+                                                        symbol3 = Some( s4 );
+                                                        let _ = self.advance();
+                                                    },
+                                                    _ =>  return Err(format!("SyntaxError at {}: Expecting ')' in functional input!", start_pos))
+                                                }
+                                            },
+                                            _ => return Err(format!("SyntaxError at {}: Expecting symbol in functional input!", start_pos))
+                                        }
+
+                                    },
+                                    _ => { }
+                                }
+                            },
+                            _ => return Err(format!("SyntaxError at {}: Expecting symbol in functional input!", start_pos))
+                        }
+                        match self.symbol.clone() {
+                            Ok(s5) => {
+                                match &*s5 {
+                                    Token::Newline(..) => {
+                                        let symbol4 = s5;
+                                        Ok(Box::new( ASTNode::Decorator(start_pos, self.lexer.get_position(), symbol1, left_node,  symbol2, right_node, symbol3, symbol4) ))
+                                    },
+                                    _ => Err(format!("SyntaxError at {}: Expecting end of file in function input!", start_pos))
+                                }
+                            },
+                            _ => Err(format!("SyntaxError at {}: Expecting symbol in functional input!", start_pos))
+                        }
+                    },
+                    _ => Err(format!("SyntaxError at {}: Expecting '@' in decorator statement!", start_pos))
+                }
+            },
+            _ => Err(format!("SyntaxError at {}: Expecting symbol in decorator statement!", start_pos))
+        }
     }
 
     fn parse_blocks_decorators(&mut self) -> Result<Box<ASTNode>, String> {
