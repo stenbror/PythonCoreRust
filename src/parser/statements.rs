69f8808fc4f8c25b234d7ca606163b29c8d596cc
Richard Magnor Stenbro <stenbror@hotmail.com>
2022-09-25 22:03:38 +0200

    Implemented UnitTests for shift right Assign statement.

--- src/parser/statements.rs
+++ src/parser/statements.rs
@@ -2506,5 +2506,48 @@ mod tests {
         }
     }
 
+    #[test]
+    fn statements_expression_bitwise_shift_right_assign() {
+        let lexer = Box::new(PythonCoreTokenizer::new("a >>= b\r\n".to_string()));
+        let mut parser = PythonCoreParser::new(lexer);
+        parser.advance();
+        let res = parser.parse_statements_stmt();
+        match &res {
+            Ok(s) => {
+                match &**s {
+                    ASTNode::SimpleStmtList(0, 10, nodes, separators, nwl) => {
+                        assert_eq!(nodes.len(), 1);
+                        let node = (*nodes[0]).clone();
+                        match node {
+                            ASTNode::ShiftRightAssignStmt( 0 , 7 , left, symbol, right) =>  {
+                                match &*left {
+                                    ASTNode::AtomName( 0, 2 , _ ) => assert!(true),
+                                    _ => assert!(false)
+                                }
+                                match &*symbol {
+                                    Token::PyShiftRightAssign(2, 5, _ ) => assert!(true),
+                                    _ => assert!(false)
+                                }
+                                match &*right {
+                                    ASTNode::AtomName( 6, 7, _ ) => assert!(true),
+                                    _ => assert!(false)
+                                }
+                                assert!(true)
+                            },
+                            _ => assert!(false)
+                        }
+                        assert_eq!(separators.len(), 0);
+                        match &**nwl {
+                            Token::Newline(7, 10, None, '\r', '\n') =>  assert!(true),
+                            _ => assert!(false)
+                        }
+                    },
+                    _ => assert!(false)
+                }
+            }
+            Err(..) => assert!(false)
+        }
+    }
+
 }
 
